f2bdf7c5863459f71a2f25750bca03eecc1a95ad
The-going <48602507+The-going@users.noreply.github.com>
2023-06-24 15:40:26 +0300

    lib: deb-build: split functionality

---
+++ lib/deb-build/preparing-distcc.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# chroot_prepare_distccd <release> <arch>
+#
+preparing_distccd() {
+	local release=$1
+	local arch=$2
+	local dest=/tmp/distcc/${release}-${arch}
+	declare -A gcc_version gcc_type
+	gcc_version['buster']='8.3'
+	gcc_version['bullseye']='9.2'
+	gcc_version['bookworm']='10.2'
+	gcc_version['sid']='10.2'
+	gcc_version['bionic']='5.4'
+	gcc_version['focal']='9.2'
+	gcc_version['hirsute']='10.2'
+	gcc_version['jammy']='12'
+	gcc_version['kinetic']='12'
+	gcc_version['lunar']='12'
+	gcc_type['armhf']='arm-linux-gnueabihf-'
+	gcc_type['arm64']='aarch64-linux-gnu-'
+	rm -f "${dest}"/cmdlist
+	mkdir -p "${dest}"
+	local toolchain_path
+	toolchain_path=$(find_toolchain "${gcc_type[${arch}]}" "== ${gcc_version[${release}]}")
+	ln -sf "${toolchain_path}/${gcc_type[${arch}]}gcc" "${dest}"/cc
+	echo "${dest}/cc" >> "${dest}"/cmdlist
+	for compiler in gcc cpp g++ c++; do
+		echo "${dest}/$compiler" >> "${dest}"/cmdlist
+		echo "${dest}/${gcc_type[$arch]}${compiler}" >> "${dest}"/cmdlist
+		ln -sf "${toolchain_path}/${gcc_type[${arch}]}${compiler}" "${dest}/${compiler}"
+		ln -sf "${toolchain_path}/${gcc_type[${arch}]}${compiler}" "${dest}/${gcc_type[${arch}]}${compiler}"
+	done
+	mkdir -p /var/run/distcc/
+	touch /var/run/distcc/"${release}-${arch}".pid
+	chown -R distccd /var/run/distcc/
+	chown -R distccd /tmp/distcc
+}
+
+ 
