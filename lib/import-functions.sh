4cf483b919df0a399dc6a4b0282a0fc41e48a1a2
The-going <48602507+The-going@users.noreply.github.com>
2023-06-24 13:37:08 +0300

    lib: extras: rename to deb-build folder

--- lib/import-functions.sh
+++ lib/import-functions.sh
@@ -4,7 +4,7 @@ while read -r file; do
 	# shellcheck source=/dev/null
 	source "$file"
 done <<< "$(
-	for d in general extras logging host bsp cli configuration compilation image rootfs main
+	for d in general deb-build logging host bsp cli configuration compilation image rootfs main
 	do
 		find "${SRC}/lib/$d" -name "*.sh"
 	done
